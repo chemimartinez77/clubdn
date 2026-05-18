c3fc50845b104cea9a7f49aaf0fe1ea46f800c77
chemimartinez77 <chemimartinez@gmail.com>
2026-05-18 19:28:43 +0200

    fix: acortando los links de whatsapp

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,6 +31,7 @@ import announcementRoutes from './routes/announcementRoutes';
 import marketplaceRoutes from './routes/marketplaceRoutes';
 import libraryLoansRoutes from './routes/libraryLoansRoutes';
 import previewRoutes from './routes/previewRoutes';
+import { previewEvent } from './controllers/previewController';
 import pushRoutes from './routes/pushRoutes';
 import photoLibraryRoutes from './routes/photoLibraryRoutes';
 import myLudotecaRoutes from './routes/myLudotecaRoutes';
@@ -188,6 +189,7 @@ app.use('/api/announcements', announcementRoutes);
 app.use('/api/marketplace', marketplaceRoutes);
 app.use('/api/library-loans', libraryLoansRoutes);
 app.use('/preview', previewRoutes);
+app.get('/e/:id', previewEvent);
 app.use('/api/push', pushRoutes);
 
 // Ruta 404
